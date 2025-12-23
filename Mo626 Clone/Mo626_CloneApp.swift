b5af17ae21fe6c4f0bfdb5204144556ace97883b
Wiza Munthali <wizamunthali@gmail.com>
2025-12-23 18:29:03 +0200

    Initial Commit

---
+++ Mo626 Clone/Mo626_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mo626_CloneApp.swift
+//  Mo626 Clone
+//
+//  Created by Wiza Munthali on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mo626_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
